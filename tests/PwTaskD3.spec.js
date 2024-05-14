9375a555c8a732bab6b2093c61b13a2ef50b3851
abdulsalam17 <abdulsalam892014@gmail.com>
2024-05-14 22:54:25 +0500

    Playwright task day3

--- tests/PwTaskD3.spec.js
+++ tests/PwTaskD3.spec.js
@@ -4,7 +4,9 @@ test ("Day3task", async({page}) => {
 
     await page.goto('https://practicetestautomation.com/practice-test-login/')
     await expect(page).toHaveTitle(/Test Login/)
+    await page.locator('id=username').click
     await page.locator('id=username').fill('student')
+    await page.locator('id=password').click
     await page.locator('id=password').fill('Password123')
     await page.getByRole('button', {type: 'submit'} ).click
     await expect(page).toHaveTitle(/Logged In Successfully | Practice Test Automation/)
