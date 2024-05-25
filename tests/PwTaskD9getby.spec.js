20b7f26c137a6a33e6493f236ad11de4e42dc92b
abdulsalam17 <abdulsalam892014@gmail.com>
2024-05-25 11:35:04 +0500

    PWTask Day9 getby

--- tests/PwTaskD9getby.spec.js
+++ tests/PwTaskD9getby.spec.js
@@ -5,7 +5,7 @@ test('Task2', async ({ page }) => {
     await page.goto('https://selectorshub.com/xpath-practice-page/');
     await expect(page).toHaveTitle(/Xpath Practice Page | Shadow dom, nested shadow dom/);
 
-    await page.pause()
+    //await page.pause()
 
     await expect(page.getByText(/Inspect this element, you will see comment just below/)).toBeVisible()
 
@@ -18,8 +18,11 @@ test('Task2', async ({ page }) => {
     await page.getByPlaceholder('Enter Password').click()
     await page.getByPlaceholder('Enter Password').fill('123456@')
 
+    // getbyrole()
+    await page.getByRole('button', {name: 'Submit'}).click()
+
     //getbylabel()
-    await page.getByLabel('Choose a car:').click()
+    await page.getByLabel('Choose a car:').selectOption('Audi')
 
     // getbyAltText All images should have an alt attribute that describes the image. You can locate an image based on the text alternative using page.getByAltText().
     await page.getByAltText('SH_512x512').click()
