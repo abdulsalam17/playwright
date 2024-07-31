cd8d2c6553251b58cc7981c5f83d79f51950ffbd
abdulsalam17 <abdulsalam892014@gmail.com>
2024-07-31 23:04:52 +0500

    31 jul

--- tests/PwTaskD2T2.spec.js
+++
@@ -1,7 +0,0 @@
-const {test,expect} = require('@playwright/test')
-
-test('Task2',async ({page}) => {
-
-    await page.goto('https://selectorshub.com/xpath-practice-page/');
-    await expect(page).toHaveTitle(/Xpath Practice Page | Shadow dom, nested shadow dom, iframe, nested iframe and more complex automation scenarios/);
-})
